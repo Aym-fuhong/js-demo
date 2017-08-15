ae3ccb25940473af52c2c5573235865252a453d6
Aym-fuhong <18829287015@163.com>
2017-08-15 19:38:36 +0800

    divide header from app

---
+++ react-todolist/view/Header.js
@@ -0,0 +1,10 @@
+const Header = React.createClass({
+
+    render: function () {
+        return <div className="title text-center">
+            <h1>todolist</h1>
+        </div>
+    }
+});
+
+export default Header;
--- react-todolist/view/index.js
+++ react-todolist/view/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const App = React.createClass({
 
     getInitialState: function () {
